e402f6591e2058ecbba01875c123fc6a5feef4a5
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-07-02 14:04:42 +0000

    [Update]admin/customerのカラムを回復、ﾏｲｸﾞﾚｰｼｮﾝまだ

--- db/migrate/20220702121926_devise_create_admins.rb
+++ db/migrate/20220702121926_devise_create_admins.rb
@@ -32,7 +32,8 @@ class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+　　　t.string :email,              null: false
+　　　t.string :encrypted_password, null: false
       t.timestamps null: false
     end
 
