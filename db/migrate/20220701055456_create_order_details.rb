d85ea7f01d7e74b9c7e08808c4c0f3373ad1ffd7
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-07-02 13:30:37 +0000

    [Update]ヒント・不要なルーティングを削除

--- db/migrate/20220701055456_create_order_details.rb
+++ db/migrate/20220701055456_create_order_details.rb
@@ -6,7 +6,7 @@ class CreateOrderDetails < ActiveRecord::Migration[6.1]
       t.integer :amount,         null: false
       t.integer :purchase_price, null: false
       t.integer :making_status,  null: false, default: 0
-      
+
       t.timestamps
     end
   end
