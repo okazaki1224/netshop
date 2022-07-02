1b493b45d3b13a23193f540515cabe42dadabf48
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-07-02 15:24:04 +0000

    [Update]旧homesｺﾝﾄﾛｰﾗ削除customerﾓﾃﾞﾙのｱｿｼｴｰｼｮﾝ回復、ﾏｲｸﾞﾚｰｼｮﾝまだ

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
-  root to: 'homes#top'
-  get 'home/about' => 'homes#about',as:'about'
+  #root to: 'homes#top'
+  #get 'home/about' => 'homes#about',as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
