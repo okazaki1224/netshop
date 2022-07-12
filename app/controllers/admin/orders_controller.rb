4d5f8dd470d013e2bcd32829c13bd3b6aa82cd67
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-07-12 18:48:19 +0000

    [Update]enumを実装（一部失敗）

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,5 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order=Order.find(params[:id])
     @customer=Customer.find(params[:id])
+    @order_details=OrderDetail.all
+    @order_detail=OrderDetail.find(params[:id])
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,11 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  enum making_status:{
+    impossible:0,
+    waiting:1,
+    making:2,
+    made_up:3
+  }
 end
