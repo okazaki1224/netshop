be7ba125ecfe47285f696a98331ab8c13ca4714b
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-07-14 12:54:34 +0000

    [Update]配送先一覧ページを途中まで

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,26 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address=Address.new
+    @addresses=Address.all
   end
 
   def edit
   end
 
   def create
+    @address=Address.new(address_params)
+    @address.save
+    redirect_to "/addresses"
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :address, :postal_code)
   end
 end
