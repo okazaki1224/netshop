747201e2693ffb57e70184ff9902ebb6bb711c34
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-07-17 07:22:35 +0000

    [Update]会員側商品詳細からカートに飛べるようにした

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items=current_customer.cart_items
+
   end
 
   def update
@@ -12,5 +14,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item=CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to "/cart_items"
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,5 +6,12 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item=Item.find(params[:id])
+    @cart_item=CartItem.new
+
+  end
+
+  private
+  def item_params
+    params.require(:items).permit(:genre_id,:name,:introduction,:image,:price)
   end
 end
