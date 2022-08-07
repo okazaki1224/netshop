6c5a82c430138b3ea16f50b46be3b321d2d29c0c
okazaki1224 <101001729+okazaki1224@users.noreply.github.com>
2022-08-07 10:40:52 +0000

    [Update]管理者と会員でログアウト後の遷移先を分けた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   ####before_action :authenticate_customer!, except:[:top,:about]
-  #def after_sign_out_path_for(resource)
-   # customer_session_path
-  #end
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      admin_session_path
+    elsif resource == :customer
+      customer_session_path
+    else
+      about_path
+    end
+  end
 
   protected
 
